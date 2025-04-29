5d856635654a0011b2661f1c1a7a767c33d985df
skad12 <stephenkunle26@gmail.com>
2025-04-29 13:05:13 +0100

    add generate FSpreview button

--- components/disbursements/Dsearch.jsx
+++ components/disbursements/Dsearch.jsx
@@ -51,7 +51,7 @@ const FloatingSearchContainer = ({ onSelectAll }) => {
   const isAnySelectionMade = isAllTransactions || searchText.trim() !== "";
 
   return (
-    <div className="flex  bg-white shadow-lg p-3 justify-between text-sm  rounded-lg w-full border border-gray-200">
+    <div className=" relati flex bg-white shadow-lg p-3 justify-between text-sm rounded-lg w-full border border-gray-200">
       {/* All Transactions Checkbox */}
       <div className="flex space-x-5">
         <div className="flex items-center ">
@@ -107,147 +107,48 @@ const FloatingSearchContainer = ({ onSelectAll }) => {
             </button>
           </div>
         )}
-
-        <button
+        {/* <button
           className="w-[150px] h-[32px] bg-blue-700 text-white text-xs rounded-sm p-1.5 hover:bg-blue-800 focus:outline-none"
           onClick={() => setIsModalOpen(true)}
         >
           Manual Disbursement
-        </button>
-
-        {isModalOpen && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-[#F1F1F1] rounded-md p-6 w-[485px] shadow-lg ">
-              <div className="flex justify-between items-center pb-4  ">
-                <h1 className="text-xl font-bold">Manual Disbursement</h1>
-                <button
-                  className="text-gray-600 hover:text-red-500"
-                  onClick={() => setIsModalOpen(false)}
-                  aria-label="Close Modal"
-                >
-                  ✕
-                </button>
-              </div>
-              <form className="space-y-4 ">
-                {/* Service Provider */}
-                <select
-                  className="w-full px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
-                  aria-label="Select Service Provider"
-                >
-                  <option value="" disabled selected>
-                    Service Provider
-                  </option>
-                  <option value="KAEDC">KAEDC</option>
-                  <option value="AEDC">AEDC</option>
-                </select>
-
-                {/* Percentage and Amount */}
-                <div className="flex items-center space-x-2">
-                  <select
-                    className="w-1/2 px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
-                    aria-label="Select Percentage or Fixed Amount"
-                  >
-                    <option value="Percentage">Percentage</option>
-                  </select>
-                  <input
-                    type="number"
-                    step="0.1"
-                    className="w-1/2 px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
-                    placeholder="1.0"
-                    aria-label="Enter Percentage"
-                  />
-                </div>
-                <input
-                  type="text"
-                  className="w-full px-4 py-2 border rounded-md bg-gray-100"
-                  placeholder="Amount "
-                  aria-readonly="true"
-                  disabled
-                />
-
-                {/* Account Details */}
-                <div className="text-sm text-gray-600">
-                  <div className="font-bold pb-2 text-center">
-                    Account Details
-                  </div>
-                  <select
-                    className="w-full mb-4 px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
-                    aria-label="Select Bank"
-                  >
-                    <option value="" disabled selected>
-                      Bank
-                    </option>
-                    <option value="GTBank">GTBank</option>
-                    <option value="Access Bank">Access Bank</option>
-                  </select>
-                  <input
-                    type="text"
-                    className="w-full mb-4 px-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
-                    placeholder="Account Number"
-                    aria-label="Enter Account Number"
-                  />
-                  <input
-                    type="text"
-                    className="w-full px-4 py-2 border rounded-md bg-gray-100"
-                    placeholder="Account Name "
-                    aria-readonly="true"
-                    disabled
-                  />
-                </div>
-
-                {/* Bank Details */}
-                <div className="text-sm text-gray-600 mt-4 text-center space-y-2">
-                  <div>Bank Name: GTBank</div>
-                  <div>Account Number: 0104647462</div>
-                  <div>Account Name: KAEDC</div>
-                </div>
-
-                {/* Buttons */}
-                <div className="flex space-x-4 pt-6 ">
-                  <button
-                    className="bg-gray-200 text-xs text-gray-600 uppercase px-6 py-3 rounded-sm w-1/2 hover:bg-gray-300"
-                    type="button"
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="bg-blue-600 text-xs text-white px-6 py-3 uppercase rounded-sm w-1/2 hover:bg-blue-700"
-                    type="submit"
-                  >
-                    Disburse Now
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        )}
-
+        </button> */}
         {/* Three-Dot Menu */}
         <div className="flex items-center justify-end " ref={dropdownRef}>
           <button
             onClick={toggleMenu}
-            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            className="p-2 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 transition"
+            aria-label="Toggle menu"
           >
             <FiMoreVertical size={20} />
           </button>
         </div>
-
         {/* Menu Dropdown */}
-        {isMenuOpen && (
-          <div className=" fixed right-2 flex-none  bg-gray-50 border border-gray-200 rounded-md shadow-lg">
-            <ul className="text-sm text-gray-700">
-              <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer border-b">
-                Disburse All Selected
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer border-b">
-                Reschedule Disbursements
-              </li>
-              <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer ">
-                Export Selected
-              </li>
-            </ul>
-          </div>
-        )}
+        {/* <div
+          className={`absolute
+            right-2 mt-10 w-44
+            bg-white border border-gray-200 rounded-lg shadow-lg
+            origin-top-right transform transition-all duration-200 ease-out 
+            ${
+              isMenuOpen
+                ? "opacity-100 scale-100"
+                : "opacity-0 scale-95 pointer-events-none"
+            }
+            z-40
+          `}
+        >
+          <ul className="text-sm text-gray-700 ">
+            <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer border-b">
+              Disburse All Selected
+            </li>
+            <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer border-b">
+              Reschedule Disbursements
+            </li>
+            <li className="py-2 px-4 hover:bg-gray-100 cursor-pointer ">
+              Export Selected
+            </li>
+          </ul>
+        </div> */}
       </div>
     </div>
   );
